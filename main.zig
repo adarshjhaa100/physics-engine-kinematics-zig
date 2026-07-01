555b105abfe635a6f277d62df5dcf8a9edc8be02
Adarsh Jha <adarshjhaa100@gmail.com>
2026-07-01 10:18:36 +0530

    Translation rectangle constant velocity

--- main.zig
+++ main.zig
@@ -5,9 +5,22 @@ const c = @cImport({
     @cInclude("SDL3/SDL_main.h");
 });
 
+const Velocity = struct {
+    vx: f32,
+    vy: f32,
+};
+
+const Position = struct {
+    x: f32,
+    y: f32,
+};
+
+const SCREEN_HEIGHT = 600;
+const SCREEN_WIDTH = 800;
 var window: ?*c.SDL_Window = null;
 var renderer: ?*c.SDL_Renderer = null;
 var prevTime: u64 = 0;
+var rectPosition: Position = .{ .x = 0, .y = 100 };
 
 // 1. THE ZIG ENTRY POINT
 // FIRST PRINCIPLE: We must satisfy Zig's runtime by providing a `pub fn main()`.
@@ -17,13 +30,24 @@ pub fn main() void {
     _ = c.SDL_EnterAppMainCallbacks(0, null, SDL_AppInit, SDL_AppIterate, SDL_AppEvent, SDL_AppQuit);
 }
 
-// pub fn getCurrentTime() i96 {
-//     var threaded_io: std.Io.Threaded = .init_single_threaded;
-//     const io = threaded_io.io();
-//     defer threaded_io.deinit();
+pub fn translate2DShape(initialPos: Position, v: Velocity, tick: f32) Position {
+    // const finalPos: Position = .{
+    //     .x = @mod((initialPosn.x + v.vx * tick), SCREEN_WIDTH - 10),
+    //     .y = @mod((initialPosn.y + v.vy * tick), SCREEN_HEIGHT - 10),
+    // };
+    //
+    std.debug.print("Velocity {}\n", .{v});
+    std.debug.print("Posn {}\n", .{initialPos});
 
-//     return std.Io.Clock.now(.awake, io).toNanoseconds();
-// }
+    // x2 = x1+ut (x1 is the position at the start, the above code was wrong)
+    const finalPos: Position = .{
+        .x = @mod((0 + v.vx * tick), SCREEN_WIDTH - 10),
+        .y = @mod((100 + v.vy * tick), SCREEN_HEIGHT - 10),
+    };
+
+    // x2 = x1 + u*t
+    return finalPos;
+}
 
 // 2. INITIALIZATION CALLBACK
 // FIRST PRINCIPLE: C Pointer Nullability.
@@ -34,7 +58,7 @@ export fn SDL_AppInit(appstate: ?*?*anyopaque, argc: c_int, argv: [*c]?[*:0]u8) 
     _ = argc;
     _ = argv;
 
-    _ = c.SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+    _ = c.SDL_SetAppMetadata("Kinematics simulation engine", "1.0", "tcsc.physics.kinematics-simulation");
 
     prevTime = c.SDL_GetTicksNS();
 
@@ -43,12 +67,13 @@ export fn SDL_AppInit(appstate: ?*?*anyopaque, argc: c_int, argv: [*c]?[*:0]u8) 
         return c.SDL_APP_FAILURE;
     }
 
-    if (!c.SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, c.SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!c.SDL_CreateWindowAndRenderer("tcsc/physics/kinematics-simulation", SCREEN_WIDTH, SCREEN_HEIGHT, c.SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         std.debug.print("Couldn't create window/renderer: {s}\n", .{c.SDL_GetError()});
         return c.SDL_APP_FAILURE;
     }
 
-    _ = c.SDL_SetRenderLogicalPresentation(renderer, 800, 600, c.SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    // logical dimensions (actual rendering "arena" space)
+    _ = c.SDL_SetRenderLogicalPresentation(renderer, SCREEN_WIDTH, SCREEN_HEIGHT, c.SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     return c.SDL_APP_CONTINUE;
 }
@@ -57,11 +82,15 @@ export fn SDL_AppInit(appstate: ?*?*anyopaque, argc: c_int, argv: [*c]?[*:0]u8) 
 export fn SDL_AppEvent(appstate: ?*anyopaque, event: ?*c.SDL_Event) c.SDL_AppResult {
     _ = appstate;
 
-    std.debug.print("EVENT: {any} /n", .{event});
+    // std.debug.print("EVENT: {any} /n", .{event});
 
     if (event) |e| {
         if (e.type == c.SDL_EVENT_QUIT) {
             return c.SDL_APP_SUCCESS;
+        }
+        // not printing anything. WHY?
+        if (e.type == c.SDL_EVENT_KEY_DOWN) {
+            std.debug.print("KEYBOARD EVENT: {any} /n", .{event});
         }
     }
     return c.SDL_APP_CONTINUE;
@@ -70,22 +99,29 @@ export fn SDL_AppEvent(appstate: ?*anyopaque, event: ?*c.SDL_Event) c.SDL_AppRes
 // 4. RENDER CALLBACK
 export fn SDL_AppIterate(appstate: ?*anyopaque) c.SDL_AppResult {
     _ = appstate;
-    const currentTime = c.SDL_GetTicksNS();
-    const elapsed = currentTime - prevTime;
-    prevTime = currentTime;
-    std.debug.print("Latency: {}ns \n", .{elapsed});
-    std.debug.print("FPS: {any} \n", .{std.math.divCeil(u64, std.math.pow(u64, 10, 9), elapsed)});
 
-    const now: f64 = @as(f64, @floatFromInt(c.SDL_GetTicks())) / 1000.0;
+    var rect: c.SDL_FRect = .{};
+    const vel: Velocity = .{ .vx = 100, .vy = 0 };
 
-    const red: f32 = @floatCast(0.5 + 0.5 * c.SDL_sin(now));
-    const green: f32 = @floatCast(0.5 + 0.5 * c.SDL_sin(now + c.SDL_PI_D * 2.0 / 3.0));
-    const blue: f32 = @floatCast(0.5 + 0.5 * c.SDL_sin(now + c.SDL_PI_D * 4.0 / 3.0));
+    // time
+    const currentTick: f32 = @as(f32, @floatFromInt(c.SDL_GetTicks())) / 1000.0; // time in seconds
+    std.debug.print("CurrentTick: {}\n", .{currentTick});
 
-    _ = c.SDL_SetRenderDrawColorFloat(renderer, red, green, blue, c.SDL_ALPHA_OPAQUE_FLOAT);
+    _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, c.SDL_ALPHA_OPAQUE); // black background
+    _ = c.SDL_RenderClear(renderer); // Start with blank screen (above line is necessary to set blank canvas)
 
-    _ = c.SDL_RenderClear(renderer);
-    _ = c.SDL_RenderPresent(renderer);
+    rectPosition = translate2DShape(rectPosition, vel, currentTick);
+
+    _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 255, c.SDL_ALPHA_OPAQUE); // blue
+    rect.x = rectPosition.x;
+    rect.y = rectPosition.y;
+    rect.w = 50;
+    rect.h = 50;
+
+    _ = c.SDL_RenderRect(renderer, &rect);
+
+    // Finalize the render on screen
+    _ = c.SDL_RenderPresent(renderer); // paint the screen
 
     return c.SDL_APP_CONTINUE;
 }
