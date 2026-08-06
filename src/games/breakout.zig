6e282322d2e21227a89459542572d29f4afd3e73
ADARSH <adarshjhaa100@gmail.com>
2026-08-06 21:51:43 +0530

    Add grid maze description

--- src/games/breakout.zig
+++ src/games/breakout.zig
@@ -1,6 +1,12 @@
 const AppState = @import("../type_schema.zig").AppState;
 const c = @import("../main.zig").c;
 
+pub const GRID_DIMENSIONS: [2]u8 = [_]u8{ 8, 6 };
+// Flattened array in
+pub const GRID_MAZE = [_]u8{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+///Setup grid in matrix format with obstacles and path.
+/// Have actors
 pub fn Initialize(app_state: *AppState, renderer: ?*c.SDL_Renderer) bool {
     // setup initial actors and objects (obstacles, moving, fixed)
     // Setup breakout actor (moving paddle)
